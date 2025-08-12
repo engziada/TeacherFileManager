f2f431332b5371ca567dbf6be10c1d6442cfd4ce
muhammad ziada <engziada@gmail.com>
2025-08-12 18:01:42 +0300

    Remove functions directory configuration and delete google-signup function

--- netlify/functions/google-signup.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event, context) => {
-  // You can access event.body and handle signup logic here
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Google signup endpoint reached" }),
-  };
-};
